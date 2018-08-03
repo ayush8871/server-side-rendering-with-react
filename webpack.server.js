016bac475bddefef78e69637d564521c7467cad4
Srivastav <Ayush.Srivastav@agilysys.com>
2018-08-03 18:50:44 +0530

    Rendering a simple component

---
+++ webpack.server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  // Tell webpack that we're going to build for nodeJS instead of browser
+  target: 'node',
+
+  // Tell webpack where is the entry file
+  entry: './src/index.js',
+
+  // Tell webpack where to put the output file that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } }]
+          ]
+        }
+      }
+    ]
+  }
+}
