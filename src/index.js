6ee21dd7d1c51e1024c6a1a9cbc38e3453801a30
Srivastav <Ayush.Srivastav@agilysys.com>
2018-08-03 19:32:13 +0530

    The public directory

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
 
-  res.send(content);
+  const html = `
+    <html>
+      <head></head>
+      <body>${content}</body>
+      <script src="bundle.js"></script>
+    </html>
+  `
+
+  res.send(html);
 })
 
 const PORT = process.env.PORT || 3000;
