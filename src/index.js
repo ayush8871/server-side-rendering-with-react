bc9323ee96ee2f682132d3c5dbd184d1ac4163e2
Ayush Srivastav <meetaayush@gmail.com>
2018-08-05 01:34:08 +0530

    Settings up Reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  // Some logic to initialize and load data in store
+
+  res.send(renderer(req, store));
 });
 
 const PORT = process.env.PORT || 3000;
